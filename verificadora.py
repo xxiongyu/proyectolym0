efbbdde323edbf141ea5c2a2ac08f6879da0a7f2
xxiongyu <x.chen@uniandes.edu.co>
2023-09-02 12:04:12 -0500

    a

--- verificadora.py
+++ verificadora.py
@@ -3,6 +3,20 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 archivo = input('ingrese el arhivo: ')
 texto = open(archivo,"r",encoding="utf-8") 
 text = texto.read()
-list = word_tokenize(text)
-print(word_tokenize(text))
+lista_word = word_tokenize(text)
+size = len(lista_word)
+dicc = {}
+for a in range(0,size):
+    b = a+1
+    c = a+2
+    word = lista_word[a]
+    if word == 'defVar':
+        variable = lista_word[b]
+        valor = lista_word[c]
+        dicc[variable] = valor
+    
+    if word == 'defProc':
+        variable = lista_word[b]
+        
+#print(lista_word)
 
